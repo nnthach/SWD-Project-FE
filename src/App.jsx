e8aa348cebf359076c300522a97fef314713d0ed
hungngo705 <hungnkse180346@fpt.edu.vn>
2025-06-23 00:30:59 +0700

    add booking consultant

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { AuthProvider } from '~/context/AuthContext';
 import { ServiceProvider } from '~/context/ServiceContext';
+import { ConsultantProvider } from '~/context/ConsultantContext';
 import { publicRouters } from '~/routes/routes';
 import ScrollToTop from './components/ScrollToTop';
 import Footer from './components/Layout/components/Footer/Footer';
@@ -11,15 +12,17 @@ function App() {
   return (
     <AuthProvider>
       <ServiceProvider>
-        <Header />
-        <ScrollToTop />
-        <Routes>
-          {publicRouters.map((route, index) => {
-            return <Route key={index} path={route.path} element={route.component} />;
-          })}
-        </Routes>
-        <Footer />
-        <ToastContainer />
+        <ConsultantProvider>
+          <Header />
+          <ScrollToTop />
+          <Routes>
+            {publicRouters.map((route, index) => {
+              return <Route key={index} path={route.path} element={route.component} />;
+            })}
+          </Routes>
+          <Footer />
+          <ToastContainer />
+        </ConsultantProvider>
       </ServiceProvider>
     </AuthProvider>
   );
