4b3850fdf2816840b9950cc5864755a30ce2a1fb
nnthach <nguyenngocthach2301@gmail.com>
2025-07-10 22:23:38 +0700

    Edit home screen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,19 @@
-import Footer from '~/components/Layout/components/Footer/Footer';
-import Header from '~/components/Layout/components/Header/Header';
+/* eslint-disable no-unused-vars */
 import styles from './Home.module.scss';
-
 import homeBanner from '~/assets/images/home_banner.jpg';
+import homeBanner2 from '~/assets/images/home_banner2.jpg';
 import ServiceCard from '~/components/ServiceCard/ServiceCard';
-import { fakeDataServiceCard } from '~/constants/fakeData';
 import { Link } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
+import { ServiceContext } from '~/context/ServiceContext';
+import BlogCard from '~/pages/Home/BlogCard/BlogCard';
 function Home() {
+  const { servicesListData, fetchAllServices } = useContext(ServiceContext);
+  console.log('servicelistdat home', servicesListData);
+
+  // useEffect(() => {
+  //   fetchAllServices();
+  // }, []);
   return (
     <div className={styles['home-wrap']}>
       <div className={styles['home-new-content']}>
@@ -19,14 +26,31 @@ function Home() {
         <div className={styles['home-service']}>
           <h1>Our Services</h1>
           <div className={styles['service-wrap']}>
-            {fakeDataServiceCard.map((item) => (
-              <ServiceCard item={item} key={item.id} />
-            ))}
+            {servicesListData && servicesListData.map((item) => <ServiceCard item={item} key={item.serviceId} />)}
           </div>
 
           <button className={styles['service-page-btn']}>
             <Link to={'/service'} style={{ textDecoration: 'none', color: 'white' }}>
               View Our Services
+            </Link>
+          </button>
+        </div>
+
+        <div className={styles['home-banner']}>
+          <img src={homeBanner2} alt="banner" />
+        </div>
+
+        <div className={styles['home-service']}>
+          <h1>Blogs</h1>
+          <div className={styles['service-wrap']}>
+            {/* {servicesListData && servicesListData.map((item) => <ServiceCard item={item} key={item.serviceId} />)} */}
+
+            <BlogCard />
+          </div>
+
+          <button className={styles['service-page-btn']}>
+            <Link to={'/blog'} style={{ textDecoration: 'none', color: 'white' }}>
+              View More Blogs
             </Link>
           </button>
         </div>
