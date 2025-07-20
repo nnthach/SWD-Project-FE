8201075f1f73f3abfccc87e6be93772135a577b1
nnthach <nguyenngocthach2301@gmail.com>
2025-07-20 21:47:01 +0700

    Edit UI

--- src/pages/Admin/components/BodyContent/StaffConsultant/ModalStaffConsultant/ModalStaffConsultant.jsx
+++ src/pages/Admin/components/BodyContent/StaffConsultant/ModalStaffConsultant/ModalStaffConsultant.jsx
@@ -106,61 +106,69 @@ function ModalStaffConsultant({
         className={styles['form-wrap']}
       >
         <h1>{formType == 'create' ? 'Create Staff' : 'Staff Detail'}</h1>
-        <div className={styles['form-input']}>
-          <label>Username</label>
-          <input
-            type="text"
-            name="username"
-            value={formConsultantData.username}
-            onChange={(e) => handleChange(e)}
-            required
-          />
+        <div className={styles.row}>
+          <div className={styles['form-input']}>
+            <label>Username</label>
+            <input
+              type="text"
+              name="username"
+              value={formConsultantData.username}
+              onChange={(e) => handleChange(e)}
+              required
+            />
+          </div>
+          <div className={styles['form-input']}>
+            <label>Fullname</label>
+            <input type="text" name="fullName" value={formConsultantData.fullName} onChange={(e) => handleChange(e)} />
+          </div>
         </div>
-        <div className={styles['form-input']}>
-          <label>Fullname</label>
-          <input type="text" name="fullName" value={formConsultantData.fullName} onChange={(e) => handleChange(e)} />
+        <div className={styles.row}>
+          <div className={styles['form-input']}>
+            <label>Email</label>
+            <input
+              type="email"
+              name="personalEmail"
+              value={formConsultantData.email}
+              onChange={(e) => handleChange(e)}
+              required
+            />
+          </div>
+          <div className={styles['form-input']}>
+            <label>Phone Number</label>
+            <input
+              type="text"
+              name="phoneNumber"
+              value={formConsultantData.phoneNumber}
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
         </div>
-        <div className={styles['form-input']}>
-          <label>Email</label>
-          <input
-            type="email"
-            name="personalEmail"
-            value={formConsultantData.email}
-            onChange={(e) => handleChange(e)}
-            required
-          />
+        <div className={styles.row}>
+          <div className={styles['form-input']}>
+            <label>Address</label>
+            <input type="text" name="address" value={formConsultantData.address} onChange={(e) => handleChange(e)} />
+          </div>
+          <div className={styles['form-input']}>
+            <label>Birthday</label>
+            <input type="date" name="birthday" value={formConsultantData.birthday} onChange={(e) => handleChange(e)} />
+          </div>
         </div>
-        <div className={styles['form-input']}>
-          <label>Phone Number</label>
-          <input
-            type="text"
-            name="phoneNumber"
-            value={formConsultantData.phoneNumber}
-            onChange={(e) => handleChange(e)}
-          />
-        </div>
-        <div className={styles['form-input']}>
-          <label>Address</label>
-          <input type="text" name="address" value={formConsultantData.address} onChange={(e) => handleChange(e)} />
-        </div>
-        <div className={styles['form-input']}>
-          <label>Birthday</label>
-          <input type="date" name="birthday" value={formConsultantData.birthday} onChange={(e) => handleChange(e)} />
-        </div>
-        <div className={styles['form-input']}>
-          <label>Gender</label>
-          <select name="gender" value={formConsultantData.gender} onChange={(e) => handleChange(e)}>
-            <option value={true}>Male</option>
-            <option value={false}>Female</option>
-          </select>
-        </div>
+        <div className={styles.row}>
+          <div className={styles['form-input']}>
+            <label>Gender</label>
+            <select name="gender" value={formConsultantData.gender} onChange={(e) => handleChange(e)}>
+              <option value={true}>Male</option>
+              <option value={false}>Female</option>
+            </select>
+          </div>
 
-        <div className={styles['form-input']}>
-          <label>Role</label>
-          <select name="roleId" value={formConsultantData.roleId} onChange={(e) => handleChange(e)}>
-            <option value={'157f0b62-afbb-44ce-91ce-397239875df5'}>Consultant</option>
-            <option value={'d5cf10f1-1f31-4016-ac13-34667e9ca10d'}>Staff</option>
-          </select>
+          <div className={styles['form-input']}>
+            <label>Role</label>
+            <select name="roleId" value={formConsultantData.roleId} onChange={(e) => handleChange(e)}>
+              <option value="157f0b62-afbb-44ce-91ce-397239875df5">Consultant</option>
+              <option value="d5cf10f1-1f31-4016-ac13-34667e9ca10d">Staff</option>
+            </select>
+          </div>
         </div>
 
         <div className={styles['submit-btn-wrap']}>
